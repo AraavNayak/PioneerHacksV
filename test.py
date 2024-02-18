fc1e739c84ef77f89681cbf41384b7306e77ed34
Araav <nayakaraav@gmail.com>
2024-02-17 16:41:32 -0800

    Update

--- test.py
+++ test.py
@@ -1,49 +1,26 @@
-import assemblyai as aai
-import pyaudio
+import speech_recognition as sr
+import subprocess
+import ffmpeg
 
-aai.settings.api_key = "71ef0a9574644b27b45cca1a8c8a86d6"
+# Initialize recognizer class                                       
+r = sr.Recognizer()
+# audio object
+path = '/Users/AraavNayak/Documents/GitHub/PioneerHacksV/files/055d6158-f16e-47f1-b2e2-53c07a6e30e2.mp3'    
 
-# Audio settings
-CHUNK = 1024  # Number of frames per audio chunk
-FORMAT = pyaudio.paInt16  # Audio format
-CHANNELS = 1  # Number of channels (mono)
-RATE = 16000  # Sample rate
 
-# instance of PyAudio to access audio input
-p = pyaudio.PyAudio()
+if path[-3:] != 'wav':
+  subprocess.call(['ffmpeg', '-i', path, 'audio.wav', '-y'])
+  path = 'audio.wav'
 
-# opening stream to record audio from the microphone
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
 
-# creating a transcriber object
-transcriber = aai.Transcriber()
+audio = sr.AudioFile(path)
+#read audio object and transcribe
 
-# start real-time transcription stream
 
-stream_id = transcriber.start_stream(config=aai.TranscriptionConfig(speaker_labels=True))
 
-try:
-    while True:
-        # Read a chunk of audio data from the microphone
-        data = stream.read(CHUNK)
-
-        # Send the audio data to AssemblyAI for transcription
-        transcriber.send_audio(stream_id, audio_data=data)
-
-        # Check for transcription results
-        results = transcriber.get_stream_results(stream_id)
-
-        # Print the transcribed text in real-time
-        for utterance in results.utterances:
-            print(f"Speaker {utterance.speaker}: {utterance.text}")
-
-except KeyboardInterrupt:
-    # Stop the stream and close the audio stream
-    transcriber.stop_stream(stream_id)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+with audio as source:
+    audio = r.record(source)                  
+    result = r.recognize_google(audio)
+    
+print(result)
+print('Done?')
